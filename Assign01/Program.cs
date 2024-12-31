0c0c5759a45d92aa3b853bc149630143efb8f9be
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-31 04:16:33 +0200

    Q2

--- Assign01/Program.cs
+++ Assign01/Program.cs
@@ -14,6 +14,45 @@
             //int result = P01.Distance();
             //Console.WriteLine($"(X-Y) = {result}"); 
             #endregion
+
+            #region 2.Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+
+            //Person[] person = new Person[3];
+
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter Data Of Person {i + 1} (Name and Age):");
+            //    person[i].Name = Console.ReadLine();
+            //    int.TryParse(Console.ReadLine(), out int age);
+            //    person[i].Age = age;
+            //}
+
+
+            //Person oldest = person[0];
+            //foreach (var p in person)
+            //{
+            //    if (p.Age > oldest.Age)
+            //    {
+            //        oldest = p;
+            //    }
+            //}
+
+
+            //Console.WriteLine($"The oldest person is {oldest.Name}, Age = {oldest.Age}");
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
