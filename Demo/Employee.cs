f04519ff3717fbc83ef9459c25394e10c2ac8e50
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-31 03:18:16 +0200

    Encapsulation

---
+++ Demo/Employee.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Employee
+    {
+
+        private int id;
+        private string name;
+        private double salary;
+
+
+        #region Apply Encapsulation User Setter Getter Method
+
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+
+        //public int GetId()
+        //{
+        //    return id; 
+        //}
+
+        //public void SetName(string name)
+        //{ 
+        //  this.name = name;
+        //}
+
+        //public string GetName()
+        //{ 
+        //    return name;
+        //}
+
+        //public void SetSalary(double salary)
+        //{ 
+        //    if(salary > 0)
+        //    this.salary = salary;
+        //}
+        //public double GetSalary()
+        //{ 
+        //   return salary;
+        //}
+
+        //public Employee(int id , string name , double salary )
+        //{
+        //    SetId(id);
+        //    SetName(name);
+        //    SetSalary(salary);
+        //}
+
+        //public override string ToString()
+        //{
+        //    return $" {id}::{name}:: {salary}";
+        //} 
+        #endregion
+
+        //public int Id
+        //{
+        //    set
+        //    {
+        //        id = value;
+        //    }
+
+        //    get
+        //    {
+        //        return id;
+        //    }
+
+        //}
+
+        //public string Name
+        //{ 
+        //  set
+        //    {
+        //        name = value;
+        //    }
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //}
+
+        //public double Salary
+        //{
+        //    set
+        //    {
+        //        if(value > 0)
+        //            salary = value; 
+        //    }
+        //    get
+        //    { 
+        //      return salary;
+        //    }
+
+
+        //}
+
+        //public string Address { set; get; }
+
+
+    }
+}
