1daac943887832ae9d7bcc44f8ade05638775b14
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-31 03:35:48 +0200

    Q1

---
+++ Assign01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assign01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            //Console.WriteLine("Enter Two Point X,Y");
+            //int.TryParse(Console.ReadLine(), out int X);
+            //int.TryParse(Console.ReadLine(), out int Y);
+
+            //Point P01 = new Point(X, Y);
+
+            //int result = P01.Distance();
+            //Console.WriteLine($"(X-Y) = {result}"); 
+            #endregion
+        }
+    }
+}
